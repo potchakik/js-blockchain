c8a88018b3441b60261b2bcf3a0090dd12842778
Dan <santhonydan705@gmial.com>
2022-07-31 17:38:26 +0800

    created levelDB

--- chain.js
+++ chain.js
@@ -1,9 +1,24 @@
 const Block = require("./block").Block;
 const BlockHeader = require("./block").BlockHeader;
 const moment = require("moment");
+const CryptoJS = require("crypto-js");
+const { Level } = require("level");
+const fs = require("fs");
+let db;
+
 const blockchain = [genesisBlock()];
 
-let genesisBlock = () => {
+let createDb = (peerId) => {
+  let dir = __dirname + "/db/" + peerId;
+
+  if (!fs.existsSync(dir)) {
+    fs.mkdirSync(dir);
+    db = new Level(dir);
+    storeBlock(getGenesisBlock());
+  }
+};
+
+let getGenesisBlock = () => {
   let blockHeader = new BlockHeader(
     1,
     null,
@@ -28,9 +43,35 @@ let addBlock = (newBlock) => {
   }
 };
 
+let getDbBlock = (index, res) => {
+  db.get(index, function (err, value) {
+    if (err) return res.send(JSON.stringify(err));
+    return res.send(value);
+  });
+};
+
 let getBlock = (index) => {
   if (blockchain.length - 1 >= index) return blockchain[index];
   else return null;
+};
+
+const generateNextBlock = (txns) => {
+  const prevBlock = getLatestBlock(),
+    prevMerkleRoot = prevBlock.blockHeader.merkleRoot;
+  (nextIndex = prevBlock.index + 1),
+    (nextTime = moment().unix()),
+    (nextMerkleRoot = CryptoJS.SHA256(1, prevMerkleRoot, nextTime).toString());
+
+  const blockHeader = new BlockHeader(
+    1,
+    prevMerkleRoot,
+    nextMerkleRoot,
+    nextTime
+  );
+  const newBlock = new Block(blockHeader, nextIndex, txns);
+  blockchain.push(newBlock);
+  storeBlock(newBlock);
+  return newBlock;
 };
 
 if (typeof exports != "undefined") {
@@ -38,4 +79,7 @@ if (typeof exports != "undefined") {
   exports.getBlock = getBlock;
   exports.blockchain = blockchain;
   exports.getLatestBlock = getLatestBlock;
+  exports.generateNextBlock = generateNextBlock;
+  exports.createDb = createDb;
+  exports.getDbBlock = getDbBlock;
 }
